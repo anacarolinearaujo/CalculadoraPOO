4cc85083250df10ffe52c6695f2ffbf73e651e9f
Ana <a.carolinearaujo97@gmail.com>
2023-12-01 00:33:16 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraPOO/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+// definição da classe calculadora
+public class Calculadora
+{
+    //Método para adição
+    public double adicao(double num1, double num2)
+    { 
+        return num1 + num2; 
+    }
+
+}
