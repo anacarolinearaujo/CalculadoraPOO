5189fe782a8b69faacc97b3c00df8b72391b9261
Ana <a.carolinearaujo97@gmail.com>
2023-12-01 11:17:40 -0300

    Criado métodos

--- CalculadoraPOO/Program.cs
+++ CalculadoraPOO/Program.cs
@@ -8,4 +8,27 @@ public class Calculadora
         return num1 + num2; 
     }
 
+    public double subtracao (double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public double multipicacao (double num1, double num2)
+    {
+        return (num1 * num2);
+    }
+
+    public double divisao (double num1, double num2)
+    {
+        if(num2 != 0)
+        {
+            return num1 / num2; 
+        }
+        else
+        {
+            Console.WriteLine("Não pode dividir por zero");
+                return double.NaN;
+        }
+    }
+
 }
